c0b152cfa3f4b4f69114ee12f2ef47063764c86e
Lars Wobus <lars.wobus@3dit.de>
2021-10-31 09:41:02 +0100

    Incomplete pause menu

---
+++ Assets/Scripts/UI/ButtonColorScheme.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class ButtonColors
+{
+    public Color normal;
+    public Color highlighted;
+    public Color pressed;
+    public Color selected;
+    public Color disabled;
+}
+
+[CreateAssetMenu(fileName = "ButtonColorScheme", menuName = "ScriptableObjects/ButtonColorScheme", order = 1)]
+public class ButtonColorScheme : ScriptableObject
+{
+    //public ButtonColors normal;
+    //public ButtonColors active;
+    public ColorBlock normal;
+    public ColorBlock active;
+}
