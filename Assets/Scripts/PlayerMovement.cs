6f11eab4dce918e912f5d6c26e241e4f08f6c27c
Steve Gotzel <steve.gotzel@3m5.de>
2021-10-31 15:57:33 +0100

    Update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,14 +13,40 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
     public Transform interactor;
-    private Vector2 movement;
-    
-        
     private Vector2 movementInput;
     
     // Update is called once per frame
-    private void Update() {
-       transform.Translate(new Vector3(movementInput.x, movementInput.y, 0) * moveSpeed * Time.deltaTime);
+    private void Update()
+    {
+        float horizontalMovement = movementInput.x;
+        float verticalMovement = movementInput.y;        
+        animator.SetFloat("Horizontal", horizontalMovement);
+        animator.SetFloat("Vertical", verticalMovement);
+        animator.SetFloat("Speed", movementInput.sqrMagnitude);
+
+        if (
+            horizontalMovement == 1
+            || horizontalMovement == -1
+            || verticalMovement == 1
+            || verticalMovement == -1
+        ) {
+            animator.SetFloat("LastHorizontal", horizontalMovement);
+            animator.SetFloat("LastVertical", verticalMovement);
+        }
+
+        // todo - check if only rotating interactor is ok
+        if (horizontalMovement > 0) {
+            interactor.localRotation = Quaternion.Euler(0, 0, 90);
+        }
+        if (horizontalMovement < 0) {
+            interactor.localRotation = Quaternion.Euler(0, 0, -90);
+        }
+        if (verticalMovement > 0) {
+            interactor.localRotation = Quaternion.Euler(0, 0, 180);
+        }
+        if (verticalMovement < 0) {
+            interactor.localRotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 
     public void OnMove(InputAction.CallbackContext ctx) {
@@ -28,6 +54,6 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void FixedUpdate() {
-        // rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime); // fixedDeltaTime -> amount of time elapsed since last time function was called
+        rb.MovePosition(rb.position + movementInput.normalized * moveSpeed * Time.fixedDeltaTime); // fixedDeltaTime -> amount of time elapsed since last time function was called
     }
 }
